b38ce12c0ff59420946e26f452fa28a1d364c808
Artyom Ulanchik <artyom.u@outlook.com>
2023-10-15 11:21:50 +0100

    project init

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import http from 'http'
+import express from 'express'
+import compression from 'compression'
+import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
+import cors from 'cors'
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors({ credentials: true }));
+app.use(cookieParser())
+app.use(compression())
+app.use(morgan('dev'))
+
+const server = http.createServer(app);
+
+export default server;
