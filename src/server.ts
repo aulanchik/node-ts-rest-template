3fdc30ab311f50a69448291d6289386f4484f247
Artyom Ulanchik <artyom.u@outlook.com>
2023-10-16 03:48:05 +0100

    code cleanup

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
-import http from 'http'
-import express from 'express'
-import compression from 'compression'
-import cookieParser from 'cookie-parser'
-import morgan from 'morgan'
-import cors from 'cors'
+import http from 'http';
+import express from 'express';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors({ credentials: true }));
-app.use(cookieParser())
-app.use(compression())
-app.use(morgan('dev'))
+app.use(cookieParser());
+app.use(compression());
+app.use(morgan('dev'));
 
 const server = http.createServer(app);
 
